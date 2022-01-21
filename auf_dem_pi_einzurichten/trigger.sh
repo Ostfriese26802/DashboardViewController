5da360e8191ab2c70adf8ba4a5199480b9027d76
janneshecht <Jannes.Hecht@gmail.com>
2022-01-21 12:27:02 +0100

    Statt echo wird ein Eintrag mittels logger generiert bei Reboot/Shutdown

--- auf_dem_pi_einzurichten/trigger.sh
+++ auf_dem_pi_einzurichten/trigger.sh
@@ -8,8 +8,8 @@ if [ -f $file ]; then
   content=$(cat $file)   # Inhalt der Datei wird eingelesen
   echo -n > $file  # Datei wird geleert
   case $content in
-    reboot   ) echo "Reboot durch DVC getriggert!" && shutdown -r now;;
-    shutdown ) echo "Shutdown durch DVC getriggert!" && shutdown now;;
+    reboot   ) logger -t DVC -p info "Reboot durch DVC getriggert!" && shutdown -r now;;
+    shutdown ) logger -t DVC -p info "Shutdown durch DVC getriggert!" && shutdown now;;
   esac
 else
   echo "Datei" $file "nicht vorhanden!"
