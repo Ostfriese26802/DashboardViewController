ca180ea4246d6f60d1895e07dbb6fb0ab7ed09b1
janneshecht <Jannes.Hecht@gmail.com>
2022-02-03 10:40:52 +0100

    Fix: Datei wird nur geleert wenn diese nicht leer ist.

--- auf_dem_pi_einzurichten/trigger.sh
+++ auf_dem_pi_einzurichten/trigger.sh
@@ -7,7 +7,7 @@ file=/home/pi/dvc/trigger
 if [ -f $file ]; then
   content=$(cat $file)   # Inhalt der Datei wird eingelesen
   sleep 5
-  echo -n > $file  # Datei wird geleert
+  [ ! -z "$content" ] && echo -n > $file  # Datei wird geleert
   case $content in
     reboot   ) logger -t DVC -p info "Reboot durch DVC getriggert!" && shutdown -r now;;
     shutdown ) logger -t DVC -p info "Shutdown durch DVC getriggert!" && shutdown now;;
